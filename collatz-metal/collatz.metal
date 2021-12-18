316222e3d388122adb75b8b53ea9ec495a38e425
skapden <skapden@gmail.com>
2021-12-18 17:03:22 -0600

    Ported collatz calc from Parallel class

---
+++ collatz-metal/collatz.metal
@@ -0,0 +1,37 @@
+//
+//  collatz.metal
+//  collatz-metal
+//
+//  Created by Sebastian Provenzano on 12/17/21.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+kernel void collatz_metal(device int*  num [[buffer(0)]],
+                          device int*  maxlen [[buffer(1)]],
+                          uint i [[thread_position_in_grid]])
+{
+    // compute sequence lengths
+    int len2 = 0;
+    if (i < *num) {
+       long val = i+1;
+       int len = 1; 
+       while (val != 1) {
+         len++;
+         if ((val % 2) == 0) {
+           val = val / 2;  // even
+         } else {
+           val = 3 * val + 1;  // odd
+         }
+       }
+        
+        // this mess right here
+        threadgroup atomic_int index;
+        atomic_store_explicit( &index, *maxlen, memory_order_relaxed );
+        // threadgroup_barrier( mem_flags::mem_none ); // do i need this?
+        atomic_fetch_max_explicit( &index, len, memory_order_relaxed ); // bad bottleneck
+        *maxlen = atomic_load_explicit( &index, memory_order_relaxed);
+        
+    }
+}
--- collatz-metal/main.swift
+++ collatz-metal/main.swift
@@ -6,6 +6,62 @@
 //
 
 import Foundation
+import MetalKit
 
-print("Hello, World!")
+collatz(tempNumSeq : 10000000) // set this to whatever you want
+
+func collatz(tempNumSeq : Int) {
+    var numSeq = tempNumSeq
+    var maxLen = 0
+    let threadWidth = numSeq
+    
+    let device = MTLCreateSystemDefaultDevice()
+    
+    print()
+    
+    let commandQueue = device?.makeCommandQueue()
+    let GPUFunctionLibrary = device?.makeDefaultLibrary()
+    let collatzGPUFunction = GPUFunctionLibrary?.makeFunction(name: "collatz_metal")
+    
+    var collatzComputePipelineState: MTLComputePipelineState!
+    do {
+        collatzComputePipelineState = try device?.makeComputePipelineState(function: collatzGPUFunction!)
+    } catch {
+        print(error)
+    }
+    
+    let numBuf = device?.makeBuffer(bytes: &numSeq, length: MemoryLayout<Int>.size, options: .storageModeShared)
+    let maxLenBuf = device?.makeBuffer(bytes: &maxLen, length: MemoryLayout<Int>.size, options: .storageModeShared)
+    
+    let commandBuf = commandQueue?.makeCommandBuffer()
+    
+    let commandEncoder = commandBuf?.makeComputeCommandEncoder()
+    commandEncoder?.setComputePipelineState(collatzComputePipelineState)
+    
+    commandEncoder?.setBuffer(numBuf, offset: 0, index: 0)
+    commandEncoder?.setBuffer(maxLenBuf, offset: 0, index: 1)
+    
+    let threadsPerGrid = MTLSize(width: threadWidth, height: 1, depth: 1)
+    let maxThreadsPerGroup = collatzComputePipelineState.maxTotalThreadsPerThreadgroup
+    let threadsPerThreadGroup = MTLSize(width: maxThreadsPerGroup, height: 1, depth: 1)
+    
+    commandEncoder?.dispatchThreads(threadsPerGrid, threadsPerThreadgroup: threadsPerThreadGroup)
+    
+    commandEncoder?.endEncoding()
+    
+    let computeStart = CFAbsoluteTimeGetCurrent()
+    commandBuf?.commit()
+    commandBuf?.waitUntilCompleted()
+    let computeEnd = CFAbsoluteTimeGetCurrent() //
+    
+    let maxLenBufPtr = maxLenBuf?.contents().bindMemory(to: Int.self, capacity: 1)
+
+    let computeElapsed = computeEnd - computeStart
+    
+    print("num is ", tempNumSeq)
+    print("max sequence length is", maxLenBufPtr!.pointee)
+    print("threadWidth is ", threadWidth)
+    print("compute time is \(String(format: "%.05f", computeElapsed))")
+
+}
 
